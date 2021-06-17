7e8cc5a245aac2bca1aa019fecb1d61684b350c3
zhiweil20 <kerryli0220@163.com>
2021-06-17 21:45:39 +0800

    first commit

---
+++ resnet18/main.py
@@ -0,0 +1,228 @@
+import os, sys, glob, shutil, json
+
+os.environ["CUDA_VISIBLE_DEVICES"] = '0'
+import cv2
+
+from PIL import Image
+import numpy as np
+from tqdm import tqdm, tqdm_notebook
+import torch
+torch.manual_seed(0)
+torch.backends.cudnn.deterministic = False
+torch.backends.cudnn.benchmark = True
+import torchvision.models as models
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+from torch.utils.data.dataset import Dataset
+
+class SVHNDataset(Dataset):
+    def __init__(self, img_path, img_label, transform=None):
+        self.img_path = img_path
+        self.img_label = img_label
+        if transform is not None:
+            self.transfom = transform
+        else:
+            self.transfom = None
+    
+    def __getitem__(self, index):
+        img = Image.open(self.img_path[index]).convert('RGB')
+
+        if self.transfom is not None:
+            img = self.transfom(img)
+
+        lbl = np.array(self.img_label[index], dtype=np.int)
+
+        lbl = list(lbl) + (5 - len(lbl)) * [10]
+
+        return img, torch.from_numpy(np.array(lbl[:5]))
+
+    def __len__(self):
+        return len(self.img_path)
+
+
+## step2 define train dataset and validate dataset
+train_path = glob.glob('../input/mchar_train/*.png')
+train_path.sort()
+train_json = json.load(open('../input/mchar_train.json'))
+train_label = [train_json[x]['label'] for x in train_json]
+print(len(train_path), len(train_label))
+
+
+train_loader = torch.utils.data.DataLoader(
+    SVHNDataset(train_path, train_label,
+                transforms.Compose([
+                    transforms.Resize((60, 120)),
+                    transforms.ToTensor(),
+                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+                ])
+    ),
+    batch_size=40,
+    shuffle=False,
+    num_workers=10,
+)
+
+
+val_path = glob.glob('../input/mchar_val/*.png')
+val_path.sort()
+val_json = json.load(open('../input/mchar_val.json'))
+val_label = [val_json[x]['label'] for x in val_json]
+print(len(val_path), len(val_label))
+
+val_loader = torch.utils.data.DataLoader(
+    SVHNDataset(val_path, val_label,
+                transforms.Compose([
+                    transforms.Resize((60,120)),
+                    transforms.ToTensor(),
+                    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+                ])
+    ),
+    batch_size=40,
+    shuffle=False,
+    num_workers=10,
+)
+
+
+## step3 define character classify model, use resnet18 as feature extract module
+class SVHN_Model1(nn.Module):
+    def __init__(self):
+        super(SVHN_Model1, self).__init__()
+        
+        model_conv = models.resnet18(pretrained=True)
+        model_conv.avgpool = nn.AdaptiveAvgPool2d(1)
+        model_conv = nn.Sequential(*list(model_conv.children())[:-1])
+        self.cnn = model_conv
+
+        self.fc1 = nn.Linear(512, 11)
+        self.fc2 = nn.Linear(512, 11)
+        self.fc3 = nn.Linear(512, 11)
+        self.fc4 = nn.Linear(512, 11)
+        self.fc5 = nn.Linear(512, 11)
+
+    def forward(self, img):
+        feat = self.cnn(img)
+        # print(feat.shape)
+        feat = feat.view(feat.shape[0], -1)
+        c1 = self.fc1(feat)
+        c2 = self.fc2(feat)
+        c3 = self.fc3(feat)
+        c4 = self.fc4(feat)
+        c5 = self.fc5(feat)
+        return c1, c2, c3, c4, c5
+
+use_cuda = False
+
+#step4 define training validate and predict module
+def train(train_loader, model, criterion, optimizer):
+    model.train()
+    train_loss = []
+
+    for i, (input, target) in enumerate(train_loader):
+        if use_cuda:
+            input = input.cuda()
+            target = target.cuda()
+
+        c0, c1, c2, c3, c4 = model(input)
+
+        loss = criterion(c0, target[:, 0]) + \
+                criterion(c1, target[:, 1]) + \
+                criterion(c2, target[:, 2]) + \
+                criterion(c3, target[:, 3]) + \
+                criterion(c4, target[:, 4])
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if i % 100 == 0:
+            print(loss.item())
+        
+        train_loss.append(loss.item())
+    return np.mean(train_loss)
+
+def validate(val_loader, model, criterion):
+    model.eval()
+    val_loss = []
+
+    with torch.no_grad():
+        for i, (input, target) in enumerate(val_loader):
+            if use_cuda:
+                input = input.cuda()
+                target = target.cuda()
+
+            c0, c1, c2, c3, c4 = model(input)
+            loss = criterion(c0, target[:, 0]) + \
+                    criterion(c1, target[:, 1]) + \
+                    criterion(c2, target[:, 2]) + \
+                    criterion(c3, target[:, 3]) + \
+                    criterion(c4, target[:, 4])
+            # loss /= 6
+            val_loss.append(loss.item())
+    return np.mean(val_loss)
+
+def predict(test_loader, model, tta=10):
+    model.eval()
+    test_pred_tta = None
+
+    for _ in range(tta):
+        test_pred = []
+        with torch.no_grad():
+            for i, (input, target) in enumerate(test_loader):
+                if use_cuda:
+                    input = input.cuda()
+                c0, c1, c2, c3, c4 = model(input)
+                output = np.concatenate([
+                    c0.data.numpy(),
+                    c1.data.numpy(),
+                    c2.data.numpy(),
+                    c3.data.numpy(),
+                    c4.data.numpy()], axis=1)
+                test_pred.append(output)
+    
+    test_pred = np.vstack(test_pred)
+    if test_pred_tta is None:
+        test_pred_tta = test_pred
+    else:
+        test_pred_tta += test_pred
+
+    return test_pred_tta
+
+
+# step5 iterate train and validate model
+model = SVHN_Model1()
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), 0.001)
+
+best_loss = 1000.0
+
+if use_cuda:
+    model = model.cuda()
+
+for epoch in range(2):
+    train_loss = train(train_loader, model, criterion, optimizer)
+    val_loss = validate(val_loader, model, criterion)
+
+    val_label = [''.join(map(str, x)) for x in val_loader.dataset.img_label]
+    val_predict_label = predict(val_loader, model, 1)
+    val_predict_label = np.vstack([
+        val_predict_label[:, :11].argmax(1),
+        val_predict_label[:, 11:22].argmax(1),
+        val_predict_label[:, 22:33].argmax(1),
+        val_predict_label[:, 33:44].argmax(1),
+        val_predict_label[:, 44:55].argmax(1),
+    ]).T
+
+    val_label_pred = []
+    for x in val_predict_label:
+        val_label_pred.append(''.join(map(str, x[x!=10])))
+    val_char_acc = np.mean(np.array(val_label_pred) == np.array(val_label))
+
+    print('Epoch: {0}, Train loss: {1} \t Val loss: {2}'.format(epoch, train_loss, val_loss))
+    print(val_char_acc)
+
+    if val_loss < best_loss:
+        best_loss = val_loss
+        torch.save(model.state_dict(), './model.pt')
